60c11225229df6b91e3c8d01214896069e511925
Pablo Orviz <orviz@ifca.unican.es>
2021-11-30 12:49:12 +0100

    Extend load_json method to support both file names and strings

--- report2sqaaas/utils.py
+++ report2sqaaas/utils.py
@@ -1,3 +1,4 @@
+import os.path
 import json
 
 from stevedore import driver, extension
@@ -26,7 +27,12 @@ def get_validator(name):
     )
 
 
-def load_json(file_name):
-    with open(file_name) as json_file:
-        json_data = json.load(json_file)
+def load_json(the_input):
+    json_data = {}
+    if os.path.isfile(the_input) and os.path.exists(the_input):
+        with open(file_name) as json_file:
+            json_data = json.load(the_input)
+    else:
+        if type(the_input) in [str]:
+            json_data = json.loads(the_input)
     return json_data
