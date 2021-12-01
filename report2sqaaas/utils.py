c1e6a9cabce5e7dd0ef90c09bd3a0bdb97c341e7
Pablo Orviz <orviz@ifca.unican.es>
2021-12-01 14:02:43 +0100

    Add some debug logging

--- report2sqaaas/utils.py
+++ report2sqaaas/utils.py
@@ -24,10 +24,13 @@ def get_validators():
     mgr = extension.ExtensionManager(
         namespace=NAMESPACE
     )
-    return dict((x.name, x.plugin) for x in mgr)
+    validators = dict((x.name, x.plugin) for x in mgr)
+    logger.debug('Found the following SQAaaS validators under namespace <%s>: %s' % (NAMESPACE, validators))
+    return validators
 
 
 def get_validator(name):
+    logger.debug('Loading validator <%s> from namespace <%s>' % (NAMESPACE, name))
     return driver.DriverManager(
         namespace=NAMESPACE,
         name=name,
@@ -38,9 +41,11 @@ def get_validator(name):
 def load_json(the_input):
     json_data = {}
     if os.path.isfile(the_input) and os.path.exists(the_input):
+        logger.debug('Loading JSON: input file found: %s' % the_input)
         with open(file_name) as json_file:
             json_data = json.load(the_input)
     else:
+        logger.debug('Loading JSON: input string found')
         if type(the_input) in [str]:
             json_data = json.loads(the_input)
     return json_data
